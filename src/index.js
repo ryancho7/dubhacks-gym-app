f01be80cf45c0431ffb78e1553efa129cd659b9c
jamesdwa <jdwa@uw.edu>
2024-10-12 14:32:54 -0700

    adding firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { initializeApp } from "firebase/app";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrwc-OcYdpfKwzBgvEcot8BVkqGaRe5Co",
+  authDomain: "dubhacks-universal-wellness.firebaseapp.com",
+  projectId: "dubhacks-universal-wellness",
+  storageBucket: "dubhacks-universal-wellness.appspot.com",
+  messagingSenderId: "72534870870",
+  appId: "1:72534870870:web:559022dbd8a864d1e664f7"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
